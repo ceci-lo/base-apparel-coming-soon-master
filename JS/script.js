54a341868d5d43901e61e9ff135b6cd9ad5ca750
Cecilia <lcecilia8147@gmail.com>
2024-07-02 17:53:31 -0300

    se agrega funcionalidad a boton enviar con js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,11 @@
 console.log("hola");
+
+let email = document.getElementById("eAddress");
+
+let boton = document.getElementById("btn");
+
+boton.addEventListener("click", (e)=>{
+    e.preventDefault();
+
+    console.log(email.value);
+});
